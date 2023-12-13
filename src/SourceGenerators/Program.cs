f896b04c4697d3c3aa12d90ec2ff490866d196c8
İbrahim Uludağ <ibrahimuludag@gmail.com>
2023-12-13 23:42:44 +0300

    Refactor

--- src/SourceGenerators/Program.cs
+++ src/SourceGenerators/Program.cs
@@ -6,10 +6,14 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        WriteViaReflection();
+    }
+
+    private static void WriteViaReflection() {
         var classTypes = Assembly.GetEntryAssembly()
-            .GetTypes()
-            .Where(c => c.IsClass && c.IsPublic)
-            .Select(c => c.FullName);
+                .GetTypes()
+                .Where(c => c.IsClass && c.IsPublic)
+                .Select(c => c.FullName);
 
         foreach (var classType in classTypes)
         {
