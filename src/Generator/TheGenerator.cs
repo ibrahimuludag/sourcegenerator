84b6a1e372ce320335182c7f5922c57bc8e6aa30
İbrahim Uludağ <ibrahimuludag@gmail.com>
2023-12-14 01:52:53 +0300

    Add WriteViaGenerator

---
+++ src/Generator/TheGenerator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Generator;
+
+[Generator]
+public class TheGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var provider = context.SyntaxProvider.CreateSyntaxProvider(
+            predicate: static(node, _) => node is ClassDeclarationSyntax,
+            transform: static (ctx, _) => (ClassDeclarationSyntax)ctx.Node
+            ).Where(c => c is not null);
+
+        var compilation = context.CompilationProvider.Combine(provider.Collect());
+
+        context.RegisterSourceOutput(compilation, Execute);
+    }
+
+    private void Execute(SourceProductionContext context, (Compilation Left, ImmutableArray<ClassDeclarationSyntax> Right) tuple)
+    {
+        var (compilation, list) = tuple;
+
+        var namelist = new List<string>();
+
+        foreach (var systax in list) {
+            var symbol = compilation.GetSemanticModel(systax.SyntaxTree)
+                .GetDeclaredSymbol(systax) as INamedTypeSymbol;
+
+            namelist.Add($"\"{symbol.ToDisplayString()}\"");
+        }
+
+        var names = string.Join(",\n ", namelist);
+
+        var theCode = $$"""
+            namespace  Generator;
+
+            public static class ClassNames {
+                public static List<string> Names = new(){
+                    {{ names }}
+                };
+            }
+            """;
+
+        context.AddSource("YourClassList.g.cs", theCode);
+    }
+}
--- src/SourceGenerators/Program.cs
+++ src/SourceGenerators/Program.cs
@@ -6,18 +6,30 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Via Reflection\n");
         WriteViaReflection();
+
+        Console.WriteLine("Via Generator\n");
+        WriteViaGenerator();
     }
 
     private static void WriteViaReflection() {
         var classTypes = Assembly.GetEntryAssembly()
                 .GetTypes()
-                .Where(c => c.IsClass && c.IsPublic)
+                .Where(c => c!= null && c.IsClass && c.IsPublic)
                 .Select(c => c.FullName);
 
         foreach (var classType in classTypes)
         {
             Console.WriteLine(classType);
         }
+    }
+
+    private static void WriteViaGenerator() {
+        foreach (var className in Generator.ClassNames.Names)
+        {
+            Console.Write(className);
+        }
+
     }
 }
